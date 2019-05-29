831eaa4bdab06251a40df43e3f997cb1132a4150
n8ta <n8@u.northwestern.edu>
2019-05-29 00:36:59 -0500

    no static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # To allow /public/uploads from carrier wave
-  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -29,8 +29,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
