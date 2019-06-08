ad78368c22fb102c2d0f671d4f84c74468198edd
n8ta <n8@u.northwestern.edu>
2019-06-08 14:32:46 -0500

    uniq

--- db/migrate/20190608005816_geography_constraints.rb
+++ db/migrate/20190608005816_geography_constraints.rb
@@ -1,7 +1,7 @@
 class GeographyConstraints < ActiveRecord::Migration[5.2]
   def change
-    add_index :species_tier1s, [:species_id, :tier1_id]
-    add_index :species_tier2s, [:species_id, :tier2_id]
-    add_index :species_tier3s, [:species_id, :tier3_id]
+    add_index :species_tier1s, [:species_id, :tier1_id], unique: true
+    add_index :species_tier2s, [:species_id, :tier2_id], unique: true
+    add_index :species_tier3s, [:species_id, :tier3_id], unique: true
   end
 end
