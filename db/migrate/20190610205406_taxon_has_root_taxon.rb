9ad0d7ce21629a441b29412627f7378fcd35d701
n8ta <n8@u.northwestern.edu>
2019-06-12 23:24:19 -0500

    taxon

--- db/migrate/20190610205406_taxon_has_root_taxon.rb
+++ db/migrate/20190610205406_taxon_has_root_taxon.rb
@@ -1,6 +1,6 @@
 class TaxonHasRootTaxon < ActiveRecord::Migration[5.2]
   def up
-    add_column :taxons, :root_taxon_id, :integer
+    # add_column :taxons, :root_taxon_id, :integer
     add_foreign_key :taxons, :taxons, column: :root_taxon_id
 
     # Taxon.all.each do |txn|
