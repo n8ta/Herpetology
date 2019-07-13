29a127212434d30271895867afc0ebd969dfb959
n8ta <n8@u.northwestern.edu>
2019-07-13 13:16:09 -0500

    prod email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,22 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "snakeid_#{Rails.env}"
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+      :address => ENV['aws_ses_address'],
+      :port => ENV['aws_ses_port'],
+      :user_name => ENV["aws_ses_user"],
+      :password => ENV["aws_ses_pass"],
+      :authentication => :login,
+      :enable_starttls_auto => true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
