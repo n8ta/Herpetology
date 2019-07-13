6857352d5c36e3d1b522f6863bfe5a6850a0f078
n8ta <n8@u.northwestern.edu>
2019-07-13 13:12:06 -0500

    email!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+      :address => ENV['aws_ses_address'],
+      :port => ENV['aws_ses_port'],
+      :user_name => ENV["aws_ses_user"],
+      :password => ENV["aws_ses_pass"],
+      :authentication => :login,
+      :enable_starttls_auto => true
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -53,7 +65,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
