24a51e863a300a4a67049647a1f164f0541cdec6
n8ta <n8@u.northwestern.edu>
2019-07-13 12:14:36 -0500

    improvements to report + scoreboard fix

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,7 +1,9 @@
+require 'quiz/quiz_controller'
 class RedirectController < ApplicationController
 
   def redir
-    redirect_to '/quiz'
+    @taxons = [Taxon.find(7), Taxon.find(5), Taxon.find(9), Taxon.find(6)]
+    render 'quiz/quiz/pick_taxon'
   end
 
 end
