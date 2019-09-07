a4f964f973fbb3f1360219da69db78fa2d02b2ce
n8ta <n8@u.northwestern.edu>
2019-09-07 00:26:43 -0500

    commas in CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,7 +9,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, "*"
   policy.object_src  :none
-  policy.script_src  :self, "unsafe-inline", "apis.google.com", "unpkg.com", "cdn.jsdelivr.net", "api.github.com", "www.google-analytics.com", "buttons.github.io", "www.googletagmanager.com"
+  policy.script_src  :self, :https, "unsafe-inline", "apis.google.com", "unpkg.com", "cdn.jsdelivr.net", "api.github.com", "www.google-analytics.com", "buttons.github.io", "www.googletagmanager.com"
   policy.style_src   :self, :https, "unsafe-inline"
   policy.connect_src :self, :https, "api.github.com"
   policy.frame_ancestors "none"
