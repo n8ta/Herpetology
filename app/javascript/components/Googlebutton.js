c2f2263197bf324d429b90b20f60bdeae2398d90
n8ta <n8@u.northwestern.edu>
2019-09-07 00:46:36 -0500

    move google image

--- app/javascript/components/Googlebutton.js
+++ app/javascript/components/Googlebutton.js
@@ -29,7 +29,7 @@ class Googlebutton extends React.Component {
     render() {
         return (
             <span className={'google_button'}>
-                <a href={"/users/auth/google_oauth2"}><img title={"Sign in with google"} src={"/assets/sign_in.png"}/></a>
+                <a href={"/users/auth/google_oauth2"}><img title={"Sign in with google"} src={"/images/sign_in.png"}/></a>
             </span>
         );
     }
