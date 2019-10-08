2d2538c7b2ea4b683352985887b7d25bfdc54210
n8ta <n8@u.northwestern.edu>
2019-10-07 23:28:38 -0500

    debug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -73,7 +73,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    flash[:notice] = "You have signed up! There is no confirmation email"
+    flash[:notice] = "You have signed up! There is no confirmation email."
+    puts "RETURN URL:"
+    puts session[:return_url]
     if session[:return_url] != nil
       return session[:return_url]
     else
