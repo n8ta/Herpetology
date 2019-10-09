d4292f725f5db4f15041f929358226ed28cc8653
n8ta <n8@u.northwestern.edu>
2019-10-09 13:25:05 -0500

    removeJWT

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@
 Devise.setup do |config|
 
 
-  config.omniauth :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], {}
+  config.omniauth :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], :skip_jwt => true
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
